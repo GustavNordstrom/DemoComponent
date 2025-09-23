c83e6d8734f8dd944c6d8506d7e42699b91dae3f
Oscar <oarbman@kth.se>
2025-09-23 14:54:20 +0200

    t

--- staticpage.py
+++ staticpage.py
@@ -1,38 +1,57 @@
 import json
 import pulumi
+from pulumi import ResourceOptions
 from pulumi_aws import s3
+from typing import Optional, TypedDict
+
+class StaticPageArgs(TypedDict):
+    index_content: pulumi.Input[str]
 
 class StaticPage(pulumi.ComponentResource):
-    def __init__(self, name: str, index_content: str, opts: pulumi.ResourceOptions | None = None):
-        super().__init__("example:StaticPage", name, None, opts)
+    # Public website URL (http://<bucket>.s3-website-<region>.amazonaws.com)
+    endpoint: pulumi.Output[str]
 
-        bucket = s3.Bucket(name)
+    def __init__(self, name: str, args: StaticPageArgs, opts: Optional[ResourceOptions] = None):
+        # Type token must align with your provider name ("static-page-component")
+        super().__init__("static-page-component:index:StaticPage", name, None, opts)
 
+        # Bucket
+        bucket = s3.Bucket(f"{name}-bucket", opts=ResourceOptions(parent=self))
+
+        # Enable static website hosting (index.html)
         website = s3.BucketWebsiteConfiguration(
             f"{name}-website",
             bucket=bucket.bucket,
             index_document={"suffix": "index.html"},
+            opts=ResourceOptions(parent=bucket),
         )
 
+        # Upload index.html
         s3.BucketObject(
             f"{name}-index",
             bucket=bucket.bucket,
             key="index.html",
-            content=index_content,
+            content=args["index_content"],
             content_type="text/html",
+            opts=ResourceOptions(parent=bucket),
         )
 
+        # Public-read policy (GET only)
         policy = bucket.bucket.apply(lambda b: json.dumps({
             "Version": "2012-10-17",
             "Statement": [{
                 "Effect": "Allow",
                 "Principal": "*",
                 "Action": "s3:GetObject",
-                "Resource": [f"arn:aws:s3:::{b}/*"],
+                "Resource": f"arn:aws:s3:::{b}/*",
             }],
         }))
-
-        s3.BucketPolicy(f"{name}-policy", bucket=bucket.bucket, policy=policy)
+        s3.BucketPolicy(
+            f"{name}-policy",
+            bucket=bucket.bucket,
+            policy=policy,
+            opts=ResourceOptions(parent=bucket),
+        )
 
         self.endpoint = website.website_endpoint
         self.register_outputs({"endpoint": self.endpoint})
